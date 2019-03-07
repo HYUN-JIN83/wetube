81b5831e63603e69f28f93c36be4fe2075f94774
KIM HYUN JIN <hjkimt@gmail.com>
2019-03-07 12:58:28 +0900

    search controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,18 @@
 export const home = (req, res) => res.render('home', {pageTitle:'Home'})
-export const search = (req, res) => res.render('search', {pageTitle: 'Search'})
+
+export const search = (req, res) => {
+    const {query: {term: searchingBy}} = req
+    //const searchingBy = req.query.term
+    res.render('search', {pageTitle: 'Search', searchingBy})
+}
+
 export const videos = (req, res) => res.render('Videos', {pageTitle: 'Video'})
+
 export const upload = (req, res) => res.render('upload', {pageTitle: 'Upload'})
+
 export const videoDetail = (req, res) => res.render('video Detail', {pageTitle: 'Video Detail'})
+
 export const editVideo = (req, res) => res.render('edit Video', {pageTitle: 'Edit Video'})
+
 export const deleteVideo = (req, res) => res.render('delete Video', {pageTitle: 'Delete Video'})
+
