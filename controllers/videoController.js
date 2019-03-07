343784c55ba5db171a0809b89824fb8ddb01fce0
KIM HYUN JIN <hjkimt@gmail.com>
2019-03-07 17:36:49 +0900

    Join : Log in HTML

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -6,8 +6,6 @@ export const search = (req, res) => {
     res.render('search', {pageTitle: 'Search', searchingBy})
 }
 
-export const videos = (req, res) => res.render('Videos', {pageTitle: 'Video'})
-
 export const upload = (req, res) => res.render('upload', {pageTitle: 'Upload'})
 
 export const videoDetail = (req, res) => res.render('video Detail', {pageTitle: 'Video Detail'})
