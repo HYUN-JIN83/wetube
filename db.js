afbfe3a4abd13ad4141dd5a9a119e3b293222121
KIM HYUN JIN <hjkimt@gmail.com>
2019-03-11 16:37:14 +0900

    Connecting to MongoDB

--- db.js
+++ db.js
@@ -1,50 +1,17 @@
-export const videos = [
+import mongoose from 'mongoose'
+
+mongoose.connect(
+    'mongodb://localhost:27017/we-tube', 
     {
-        id: 324393,
-        title: 'Video awesome',
-        description: 'this is something I love',
-        views: 24,
-        videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-        creator:{
-            id:121212,
-            name: 'HyunJin',
-            email: 'hjkimt@abc.com'
-        }
-    },
-    {
-        id: 13123,
-        title: 'Video super',
-        description: 'this is something I love',
-        views: 24,
-        videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-        creator:{
-            id:121212,
-            name: 'HyunJin',
-            email: 'hjkimt@abc.com'
-        }
-    },
-    {
-        id: 3545345,
-        title: 'Video nice',
-        description: 'this is something I love',
-        views: 24,
-        videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-        creator:{
-            id:121212,
-            name: 'HyunJin',
-            email: 'hjkimt@abc.com'
-        }
-    },
-    {
-        id: 322112,
-        title: 'Video perfect',
-        description: 'this is something I love',
-        views: 24,
-        videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-        creator:{
-            id:121212,
-            name: 'HyunJin',
-            email: 'hjkimt@abc.com'
-        }
-    },
-]
+        useNewUrlParser: true,
+        useFindAndModify: false
+    }
+)
+
+const db = mongoose.connection
+
+const handleOpen = () => console.log('Connected to DB')
+const handleError = error => console.log(`X Error on DB Connection:${error}`)
+
+db.once('open', handleOpen)
+db.on('error', handleError)
