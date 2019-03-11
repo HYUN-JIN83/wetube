609bdc39a0620268158f84a00cd25c674102e025
KIM HYUN JIN <hjkimt@gmail.com>
2019-03-11 11:59:00 +0900

    home controller

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id: 324393,
+        title: 'Video awesome',
+        description: 'this is something I love',
+        views: 24,
+        videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+        creator:{
+            id:121212,
+            name: 'HyunJin',
+            email: 'hjkimt@abc.com'
+        }
+    },
+    {
+        id: 13123,
+        title: 'Video super',
+        description: 'this is something I love',
+        views: 24,
+        videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+        creator:{
+            id:121212,
+            name: 'HyunJin',
+            email: 'hjkimt@abc.com'
+        }
+    },
+    {
+        id: 3545345,
+        title: 'Video nice',
+        description: 'this is something I love',
+        views: 24,
+        videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+        creator:{
+            id:121212,
+            name: 'HyunJin',
+            email: 'hjkimt@abc.com'
+        }
+    },
+    {
+        id: 322112,
+        title: 'Video perfect',
+        description: 'this is something I love',
+        views: 24,
+        videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+        creator:{
+            id:121212,
+            name: 'HyunJin',
+            email: 'hjkimt@abc.com'
+        }
+    },
+]
