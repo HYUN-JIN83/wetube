50f6c5d2ce111628b9342b9ca19e035f748b5c36
KIM HYUN JIN <hjkimt@gmail.com>
2019-03-12 11:19:54 +0900

    uploading and creating a video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,15 @@
 import routes from '../routes'
-export const home = (req, res) => {
-    res.render('home', {pageTitle:'Home', videos})
+import Video from '../models/Video'
+
+export const home = async(req, res) => {
+    try{
+        const videos = await Video.find({})
+        res.render('home', {pageTitle:'Home', videos})
+    } catch(error){
+        console.log(error)
+        res.render('home', {pageTitle:'Home', videos: [] })
+    }
+    
 }
 
 export const search = (req, res) => {
@@ -11,12 +20,17 @@ export const search = (req, res) => {
 export const getUpload = (req, res) => 
     res.render('upload', {pageTitle: 'Upload'})
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
     const {
-        body: {file, title, description}
+        body: {title, description},
+        file: {path}
     } = req
-    // 비디오 업로드 및 저장
-    res.redirect(routes.videoDetail(3545345))
+    const newVideo = await Video.create({
+        fileUrl: path,
+        title,
+        description
+    })
+    res.redirect(routes.videoDetail(newVideo.id))
 }
 
 export const videoDetail = (req, res) => res.render('videoDetail', {pageTitle: 'Video Detail'})
